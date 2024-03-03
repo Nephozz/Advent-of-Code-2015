886c93c6238e62933189f7ee0d68994a4c979ea6
Thomas Bocandé <thomasbocande@gmail.com>
2024-03-03 22:47:02 +0100

    try day2

--- Day 2.cpp
+++ Day 2.cpp
@@ -2,24 +2,45 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    std::ifstream inputFile ("inputs/input_Day1");
-    
-    string input;
-    vector<string> sizes;
+    std::ifstream inputFile ("inputs/input_Day2");
 
-    if (inputFile.is_open()) {
-        while (inputFile) {
-            sizes.push_back (getline(inputFile,input));
-        }
-        
 
-        inputFile.close();
-    } else {
-        cout << "Failed to open the file !" << endl;
+    if (!inputFile.is_open()) {
+        cout << "Error: File not found" << endl;
+        return 1;
     }
+
+    vector<int> sizes;
+    string line;
+
+    while (getline(inputFile, line)) {
+        istringstream iss(line);
+
+        vector<int> currentSize;
+        string size;
+        while (getline(iss, size, 'x')) {
+            currentSize.push_back(stoi(size));
+        }
+
+        sort(currentSize.begin(), currentSize.end());
+
+        sizes.push_back(3 * currentSize[0] * currentSize[1] + 2 * currentSize[1] * currentSize[2] + 2 * currentSize[0] * currentSize[2]);
+        cout << "Current: " << sizes.back() << endl;
+    }
+
+    int total = 0;
+    for (int size : sizes) {
+        total += size;
+    }
+
+    cout << "Total: " << total << endl;
+
+    inputFile.close();
 
     return 0;
 }
